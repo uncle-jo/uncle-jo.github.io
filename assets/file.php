8e31cd6f8c6bbd7dcc9eadbf90e019ccc9b51f68
Timoshenko Kostya <it.kostya.tim@gmail.com>
2018-07-18 12:37:03 +0300

    first commit

---
+++ assets/file.php
@@ -0,0 +1,19 @@
+<?php
+// $data = $_POST;
+// if(isset($data['submit'])) {
+// 	$contactName = $data['contactName'];
+// 	$contactSubject = $data['contactSubject'];
+// 	$contactMessage = $data['contactMessage'];
+// 	$contactEmail = $data['contactEmail'];
+// 	$file = fopen("message.txt","a+"); 
+// 	$text="Вам пришло письмо от пользователя: " . $contactName 
+// 			. "Вам пришло: " . $contactSubject 
+// 			. "Текст сообщения: " . $contactMessage 
+// 			. "Ответить можно на адрес электронной почты: " . $contactEmail;
+// 	flock($file, LOCK_EX);
+// 	fwrite($file, $text);
+// 	flock($file, LOCK_UN);
+// 	fclose($file);
+// 	echo "Сообщение отправленно.";	
+// }
+?>
